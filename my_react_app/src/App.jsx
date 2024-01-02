3c14f817441df48c14d8cbe231bb6f50ef6f1b29
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-01-02 15:35:00 +0530

    My First React project

---
+++ my_react_app/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div>
+      <h1>Hey I am Learning React</h1>
+      <p>
+        I am Divyang Chhantbar and started learning React from <a href="https://youtube.com/playlist?list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&si=LggF6p1rKk0Vq5Rt" target="Chai Aur Code">Chai Aur Code
+        </a> <br />
+        Where I have learn about <ol>
+          <li>Frameworks and Libraries difference
+</li>
+          <li>React :React dom and React native
+</li>
+          <li>Bundlers </li>
+          <li>Json and json packages </li>
+          <li>How to create a project and run in the environment and Npm and npx and jsx</li>
+        </ol>
+        Goto my Twitter handle which is  <a href="https://twitter.com/DChhantbar">@DChhantbar</a> to see the thread on what i am learning.
+      </p>
+    </div>
+  );
+}
+
+export default App
