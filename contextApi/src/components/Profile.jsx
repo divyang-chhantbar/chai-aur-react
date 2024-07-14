29141011ea8de0738a53fbc1f86a2066d76f2bfa
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-07-14 22:27:09 +0530

    fixed some bugs

--- contextApi/src/components/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React , {useContext} from "react";
-import userContext from "../context/Context"
-
-function Profile() {
-    const {user} = useContext(userContext)
-    if(!user) return<div>Please Login</div>            
-    return <div>{`Welcome ${user} `}</div>
-}
-export default Profile
